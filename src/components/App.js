0a078854be2c3ebf15e600d19e3b0935c1930230
Akhil <bnsakhil36@gmail.com>
2026-04-26 10:56:39 +0530

    Completed Lift State Up 5 assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const LoginForm = ({ isLoggedIn, setIsLoggedIn }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+    
+  };
+
+  if (isLoggedIn) {
+    return <h2>You are logged in</h2>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" />
+      <br/>
+      <br/>
+      <input type="password" placeholder="Password" />
+      <br/>
+      <br/>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <LoginForm
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
